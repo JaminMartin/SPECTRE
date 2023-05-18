921a3a60c0e9831b7a132343782ce43b4525ae5a
JaminMartin <jamin.martin1@gmail.com>
2023-05-18 17:00:46 +1200

    major improvements to gui still wip

--- src/Emission_GUI_HR650.py
+++ src/Emission_GUI_HR650.py
@@ -1,91 +1,201 @@
+# Importing tkinter module
 import tkinter as tk
-import matplotlib.pyplot as plt
 import numpy as np 
 import ttkbootstrap as tb
 import pandas as pd        
 from HR640_Driver import HR640_Spectrometer
 import matplotlib.pyplot as plt
-import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-import numpy as np
 from ttkbootstrap.constants import *
-import ttkbootstrap as tb
+from time import sleep
+
+
+"""
+Def Plot
+"""
 plt.style.use('dark_background')
-
-root = tb.Window(themename="dracula")    
-root.title('HR660 Control')
-
-
-
 fig = Figure(figsize=(4, 4), dpi = 200)
 ax = fig.add_subplot(111)
 ax.clear
 ax.set_facecolor('#282a36')
-x = np.linspace(0, 2*np.pi, 1000)
-y = np.sin(x)
+x = 0
+y = 0
+ax.set_xlim(500,600)
+ax.set_ylim(0,1000)
 line, = ax.plot(x, y, color = '#bd93f9')
 fig.patch.set_facecolor('#282a36')
 ax.tick_params(color='#ffb86c', labelcolor='#ffb86c')
 for spine in ax.spines.values():
         spine.set_edgecolor('#ffb86c')
+"""
+Global Vars
+"""
+spectrometer = None
+lockin = None
+s_range = None
+iterator = 0 
+y_to_plot = []
+x_to_plot = []
+interupt_type = None
+def validate_number(x) -> bool:
+    """Validates that the input is a number"""
+    if x.isdigit():
+        return True
+    elif x == "":
+        return False
+    else:
+        return False
 
+def get_spectrometer_status():
+    global spectrometer
+    spectrometer = HR640_Spectrometer(emulate=True)
+    if spectrometer.emulation == True:
+      status_var.set('Emulated')
+    else:
+     status_var.set('Initialised')  
+    
+         
+       
+def get_spectrometer_wl():
+    global spectrometer
+    wl = spectrometer.get_wavelength()   
+    current_wl_var.set(wl)
 
-
-label = tb.Label( text='Frequency:').pack(side=LEFT)
-
-frequency = tb.Scale(value=1, from_=1, to=50,bootstyle="light")
-frequency.pack(side=LEFT, fill=X, expand=YES, padx=5)
-
-label2 = tb.Label(text='Amplitude:').pack(side=LEFT)
-
-amplitude = tb.Scale(value=0.5, from_=0, to=10,bootstyle="light")
-amplitude.pack(side=LEFT, fill=X, expand=YES, padx=5)
-
-canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.draw()
-canvas.get_tk_widget().pack(expand=YES)
-phase = 0
-
-def update_plot():
-    freq = frequency.get()
-    amp = amplitude.get()
-    line.set_ydata(amp * np.sin(freq * x + update_plot.i/10.0))
-    ax.draw_artist(ax.patch)
-    ax.draw_artist(line)
-    fig.canvas.blit(ax.bbox)
-    update_plot.i += 1
-
-update_plot.i = 0
+def go_to_wavelength():
+    go_to = go_to_var.get()
+    if validate_number(go_to) == True:
+        spectrometer.goto_wavelength(go_to)
+    else:
+        pass    
+def scan_range():
+    global s_range
+    s_range = None
+    start_wl = start_var.get()
+    stop_wl = stop_var.get()
+    step_wl = step_var.get()
+    if validate_number(start_wl) == True and validate_number(stop_wl) == True and validate_number(step_wl) == True:
+        s_range = np.arange(float(start_wl),float(stop_wl),float(step_wl))
+    else:
+        print('invalid inputs')    
 
 def start():
-    if not animate.running:
-        animate.running = True
-        line.set_visible(True)
-        animate()
-
+        global interupt_type 
+        global x_to_plot
+        global y_to_plot
+        if interupt_type == 'stop':
+            y_to_plot = []
+            x_to_plot = []
+        else:    
+            if not scan.running:
+                scan.running = True
+                scan()
+        
 
 def stop():
-    animate.running = False 
+    global interupt_type
+    interupt_type = 'stop'
+    scan.running = False 
+    start_var.set('')
+    stop_var.set('')
+    step_var.set('')
 
-def save():
-    filename = name_entry.get()
-    if filename:
-        fig.savefig(filename + '.svg')    
-def animate():
-    if animate.running:
+
+
+def scan():
+    try:
+        scan_range()
+        global s_range
+        global iterator
         update_plot()
-        root.after(1, animate)   
-animate.running = False         
-start_label = tb.Button(root, text='Start', command= start,bootstyle="success-outline")
-start_label.pack(side=LEFT,fill=X, expand=YES, padx=5)
-stop_label = tb.Button(root, text='Stop', command= stop,bootstyle="warning-outline")
-stop_label.pack(side = LEFT,fill=X, expand=YES, padx=5, )
-label2 = tb.Label(text='File Name:').pack(side=LEFT)
-name_entry = tb.Entry(root)
-name_entry.pack(side = LEFT,fill=X, expand=YES, padx=5)
+        if scan.i < len(s_range):
+            if scan.running:
+                
+                scan.i += 1
+                iterator += 1
+                master.after(1000, scan)  
+        else:
+            scan.running = False 
+            print('scan finished')
+    except:
+        scan.running = False   
+        pass   
+ 
 
-save_button = tb.Button(root, text="Save", command=save,bootstyle="danger-outline")
-save_button.pack(fill=tk.X)
-line.set_visible(False)
-root.mainloop()
+def update_plot():
+    try:
+        global s_range
+        global x_to_plot
+        global y_to_plot
+    
+        x = s_range[iterator]
+        y = s_range[iterator]*1
+        x_to_plot.append(x)
+        y_to_plot.append(y)
+        line.set_ydata(y_to_plot)
+        line.set_xdata(x_to_plot)
+        ax.draw_artist(ax.patch)
+        ax.draw_artist(line)
+        fig.canvas.blit(ax.bbox) 
+    except:
+        pass    
+
+# creating Tk window and Tk Vars
+master = tb.Window(themename="dracula")  
+status_var = tk.StringVar()
+status_var.set('Uninitialised')
+current_wl_var = tk.StringVar()  
+current_wl_var.set('')
+
+start_var = tk.StringVar()  
+start_var.set('')
+stop_var = tk.StringVar()  
+stop_var.set('')
+step_var = tk.StringVar()  
+step_var.set('')
+
+
+
+go_to_var = tk.StringVar(value='')
+pane = tk.Frame(master)
+pane.grid(row=0, column=0, padx=10, pady=5)
+pane2 = tk.Frame(master)
+pane2.grid(row=0, column=1, padx=10, pady=5)
+
+
+# General layout of GUI
+
+spec_init_button = tb.Button(pane, text='Initialise Spectrometer', command = get_spectrometer_status).grid(row=0, column=0, padx=5, pady=5)
+spec_status_label = tb.Label(pane,textvariable=status_var,).grid(row=0, column=1, padx=5, pady=5)
+
+
+spec_wl_button = tb.Button(pane, text='Get Current Wavelength', command = get_spectrometer_wl).grid(row=1, column=0, padx=5, pady=5)
+current_wl_label = tb.Label(pane,textvariable=current_wl_var,).grid(row=1, column=1, padx=5, pady=5)
+
+
+spec_go_button = tb.Button(pane, text='Go to Wavelength', command = go_to_wavelength).grid(row=2, column=0, padx=5, pady=5)
+go_to_entry = tb.Entry(pane, textvariable = go_to_var).grid(row=2, column=1, padx=5, pady=5)
+
+
+start_scan = tb.Button(pane, text='Start Scan',command = start ).grid(row=6, column=0, padx=5, pady=5)
+stop_scan = tb.Button(pane, text='Stop Scan',command = stop).grid(row=6, column=1, padx=5, pady=5)
+start_params = tb.Label(pane,text='Start Wavelength',).grid(row=3, column=0, padx=5, pady=5)
+start_entry = tb.Entry(pane, textvariable = start_var).grid(row=3, column=1, padx=5, pady=5)
+stop_params = tb.Label(pane,text='Stop Wavelength',).grid(row=4, column=0, padx=5, pady=5)
+stop_entry = tb.Entry(pane, textvariable = stop_var).grid(row=4, column=1, padx=5, pady=5)
+step_params = tb.Label(pane,text='Step Size',).grid(row=5, column=0, padx=5, pady=5)
+step_entry = tb.Entry(pane, textvariable = step_var).grid(row=5, column=1, padx=5, pady=5)
+"""
+Define Figure params
+"""
+
+
+
+canvas = FigureCanvasTkAgg(fig, master=pane2)
+canvas.draw()
+canvas.get_tk_widget().grid(row =7)
+phase = 0
+# Execute Tkinter
+scan.i = 0  
+scan.running = False   
+master.mainloop()
