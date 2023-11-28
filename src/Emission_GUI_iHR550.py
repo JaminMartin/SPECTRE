5fa7b08b3b7b5b0931f9dad7e67d1eaa178a4a0e
JaminMartin <jamin.martin1@gmail.com>
2023-11-29 09:32:25 +1300

    tidied up project for refactoring

--- src/Emission_GUI_iHR550.py
+++
@@ -1,5 +0,0 @@
-import tkinter as tk
-import matplotlib.pyplot as plt
-import numpy as np 
-import ttkbootstrap as tb
-import pandas as pd
---
+++ src/Photon_Counter_Driver.py
@@ -0,0 +1,2 @@
+# currently a work in progress, the photon counter driver can be found in development here:
+# https://github.com/JaminMartin/C8855-PhotonCounter
